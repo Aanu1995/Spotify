eb87abb4875546108f60297245560bef4dddfb99
Aanu Olakunle <olakunleaanu@gmail.com>
2021-04-08 21:25:09 +0100

    remove key

--- Spotify/Managers/PrivateKeys.swift
+++ Spotify/Managers/PrivateKeys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct PrivateKeys {
-  static let clientId = "f31b0df089d84ae5a377152080005fd1"
-  static let clientSecret = "a8dd4d70b8364ea2b0ca103fa237469a"
+  static let clientId = ""
+  static let clientSecret = ""
 }
